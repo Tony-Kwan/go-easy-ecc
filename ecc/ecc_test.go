087008e39985117c640c692d5fd3306a45fc424f
Tony-Kwan <15813669070@163.com>
2019-02-10 21:10:57 +0800

    test code

--- ecc/ecc_test.go
+++ ecc/ecc_test.go
@@ -1,8 +1,17 @@
 package ecc
 
 import (
+	"crypto/sha256"
 	"encoding/hex"
+	"io"
+	"io/ioutil"
+	"os"
 	"testing"
+)
+
+const (
+	pubKeyHex = "0298dd2b270a990d6750a3bbbd466e9570f358c4935e8cc6c85157f60398de3549"
+	priKeyHex = "6adcaf573be5e8d56a27122a942b1a5b2afc137210afe10646a2e412a19ba026"
 )
 
 func TestGetEccCurve(t *testing.T) {
@@ -21,8 +30,6 @@ func TestMakeKey(t *testing.T) {
 }
 
 func TestSign(t *testing.T) {
-	pubKeyHex := "0298dd2b270a990d6750a3bbbd466e9570f358c4935e8cc6c85157f60398de3549"
-	priKeyHex := "6adcaf573be5e8d56a27122a942b1a5b2afc137210afe10646a2e412a19ba026"
 	hashHex := "7066479555585967030553493694485122506577956075909457428640587478"
 	pubKeySlice, _ := hex.DecodeString(pubKeyHex)
 	priKeySlice, _ := hex.DecodeString(priKeyHex)
@@ -46,8 +53,6 @@ func TestSign(t *testing.T) {
 }
 
 func TestVerify(t *testing.T) {
-	pubKeyHex := "0298dd2b270a990d6750a3bbbd466e9570f358c4935e8cc6c85157f60398de3549"
-	priKeyHex := "6adcaf573be5e8d56a27122a942b1a5b2afc137210afe10646a2e412a19ba026"
 	hashHex := "7066479555585967030553493694485122506577956075909457428640587478"
 	pubKeySlice, _ := hex.DecodeString(pubKeyHex)
 	priKeySlice, _ := hex.DecodeString(priKeyHex)
@@ -77,4 +82,101 @@ func TestVerify(t *testing.T) {
 	t.Log("Hash:", hex.EncodeToString(hash[:]))
 	t.Log("Sign:", hex.EncodeToString(sign[:]))
 	t.Log("Valid:", valid)
+}
+
+func TestSignFile(t *testing.T) {
+	pubKeySlice, _ := hex.DecodeString(pubKeyHex)
+	priKeySlice, _ := hex.DecodeString(priKeyHex)
+	var pubKey [EccBytes + 1]uint8
+	var priKey [EccBytes]uint8
+	copy(pubKey[:], pubKeySlice[:EccBytes+1])
+	copy(priKey[:], priKeySlice[:EccBytes])
+
+	f, err := os.Open("/Users/ZhongXian.Guan/Documents/pw/fm.txt")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer f.Close()
+
+	buf := make([]byte, 1024)
+	var hash [EccBytes]uint8
+	signFile, _ := os.Create("/Users/ZhongXian.Guan/go/src/github.com/Tony-Kwan/go-easy-ecc/ecc/test.bin")
+	defer signFile.Close()
+	for i := 0; ; i++ {
+		n, err := f.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			t.Error(err)
+			return
+		}
+
+		h := sha256.New()
+		h.Write(buf[:n])
+		hashSlice := h.Sum(nil)
+		copy(hash[:], hashSlice[:EccBytes])
+		t.Log(i, hex.EncodeToString(hash[:]))
+		sign, err := Sign(priKey, hash)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		_, err = signFile.Write(sign[:])
+		t.Log(hex.EncodeToString(sign[:]))
+		if err != nil {
+			t.Error(err)
+			return
+		}
+	}
+}
+
+func TestVerifyFile(t *testing.T) {
+	pubKeySlice, _ := hex.DecodeString(pubKeyHex)
+	priKeySlice, _ := hex.DecodeString(priKeyHex)
+	var pubKey [EccBytes + 1]uint8
+	var priKey [EccBytes]uint8
+	copy(pubKey[:], pubKeySlice[:EccBytes+1])
+	copy(priKey[:], priKeySlice[:EccBytes])
+
+	signBytes, err := ioutil.ReadFile("test.bin")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	f, err := os.Open("/Users/ZhongXian.Guan/Documents/pw/fm.txt")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+	defer f.Close()
+
+	buf := make([]byte, 1024)
+	var hash [EccBytes]uint8
+	var sign [EccBytes * 2]uint8
+	for i := 0; ; i++ {
+		m, err := f.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			t.Error(err)
+			return
+		}
+		h := sha256.New()
+		h.Write(buf[:m])
+		hashSlice := h.Sum(nil)
+		copy(hash[:], hashSlice[:EccBytes])
+		t.Log(i, hex.EncodeToString(hash[:]))
+
+		copy(sign[:], signBytes[i*EccBytes*2:(i+1)*EccBytes*2])
+		verify, err := Verify(pubKey, hash, sign)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		t.Log(verify)
+	}
 }
